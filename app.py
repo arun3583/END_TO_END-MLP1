c4303ba946f23c6045c992bf73a359a79a2d2217
arun3583 <arunaswin3583@gmail.com>
2023-09-08 08:27:40 +0530

    Web app is completed

--- app.py
+++ app.py
@@ -21,6 +21,14 @@ def predict_api():
     output=model.predict(output_data)
     print(output[0])
     return jsonify(output[0])
+@app.route("/predict", methods=["POST"])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=model.predict(final_input)
+    print(output[0])
+    return render_template("home.html", output="The Predicted Price is {}".format(output[0]))
 
 if __name__ == "__main__":
-    app.run(debug=True,port=5002)
+    app.run(debug=True,port=5003)
